19e9fd6c09c54768561f162940ca06ab5a9da5e0
Jasper Alblas <just_jasper@hotmail.com>
2024-07-16 20:20:27 +0200

    Implements game loop in console

--- js/script.js
+++ js/script.js
@@ -1,42 +1,5 @@
-const GameController = (function () {
-    const winConditions = [
-        [0, 1, 2], // Top row
-        [3, 4, 5], // Middle row
-        [6, 7, 8], // Bottom row
-        [0, 3, 6], // Left column
-        [1, 4, 7], // Middle column
-        [2, 5, 8], // Right column
-        [0, 4, 8], // Top-left to bottom-right diagonal
-        [2, 4, 6]  // Top-right to bottom-left diagonal
-    ];
-
-    const playes = [];
-    const currentPlayer = 'X';
-
-    const checkForWin = () => {
-        console.log("CHECKING WIN")
-        const board = GameBoard.returnBoard()
-        for (let condition of winConditions) {
-            const [a, b, c] = condition;
-            if (board[a] !== null && board[b] !== null && board[c] !== null && board[a] === board[b] && board[a] === board[c]) {
-                return board[a]; // Return 'X' or 'O'
-            }
-        }
-        return null;
-    }
-
-    const checkForDraw = () => {
-        if (!checkForWin() && GameBoard.returnBoard().every(cell => cell != "")) {
-            alert("DRAW");
-            return true;
-        }
-    }
-
-    return { checkForWin, checkForDraw };
-})();
 
 const GameBoard = (function () {
-    console.log("CALLED")
     const board = Array(9).fill(null);
 
     const returnBoard = () => board;
@@ -53,16 +16,61 @@ const GameBoard = (function () {
         return false;
     }
 
-    const isValidMode = () => {
+    const isValidNode = () => {
         return true;
     }
 
     const printInfo = () => console.log(board);
 
-    return { returnBoard, resetBoard, placeMark, printInfo };
+    return { returnBoard, resetBoard, placeMark, isValidNode, printInfo };
 })();
 
+const GameController = (function () {
+    const winConditions = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Top-left to bottom-right diagonal
+        [2, 4, 6]  // Top-right to bottom-left diagonal
+    ];
+
+    //const players = [];
+    let currentPlayer = 'X';
+    const board = GameBoard.returnBoard();
+
+    const checkForWin = () => {
+        console.log("CHECKING WIN")
+        for (let condition of winConditions) {
+            const [a, b, c] = condition;
+            if (board[a] !== null && board[b] !== null && board[c] !== null && board[a] === board[b] && board[a] === board[c]) {
+                return board[a]; // Return 'X' or 'O'
+            }
+        }
+        return null;
+    }
+
+    const checkForDraw = () => {
+        if (board.every(cell => cell != null)) {
+            alert("DRAW");
+            return true;
+        }
+    }
+
+    const playGame = () => {
+        var input = prompt("Which cell to add mark?");
+        GameBoard.placeMark(input, currentPlayer);
+        currentPlayer = currentPlayer == "X" ? "O" : "X";
+    }
+
+    return { checkForWin, checkForDraw, playGame };
+})();
+
+
 const DisplayController = (function () {
+
     return {};
 })();
 
@@ -71,21 +79,28 @@ function createPlayer(name) {
     return { name, score };
 }
 
-console.log("!!!")
-//while (!GameController.checkForWin()) {
-console.log("CALLED?")
-GameBoard.placeMark(0, 'X');
-GameBoard.placeMark(1, 'O');
-GameBoard.placeMark(2, 'X');
-GameBoard.placeMark(3, 'O');
-GameBoard.placeMark(4, 'O');
-GameBoard.placeMark(5, 'O');
-GameBoard.placeMark(6, 'X');
-GameBoard.placeMark(7, 'O');
-GameBoard.placeMark(8, 'X');
-//break;
-//}
-GameController.checkForDraw();
+
+
+while (!GameController.checkForWin() && !GameController.checkForDraw()) {
+
+    GameController.playGame();
+    GameBoard.printInfo();
+    /*
+    console.log("CALLED?")
+    GameBoard.placeMark(0, 'X');
+    GameBoard.placeMark(1, 'O');
+    GameBoard.placeMark(2, 'X');
+    GameBoard.placeMark(3, 'O');
+    GameBoard.placeMark(4, 'O');
+    GameBoard.placeMark(5, 'O');
+    GameBoard.placeMark(6, 'X');
+    GameBoard.placeMark(7, 'O');
+    GameBoard.placeMark(8, 'X');
+    //break;
+    //}
+    */
+}
+
 
 
 const winner = GameController.checkForWin();
@@ -95,4 +110,3 @@ if (winner) {
 } else {
     console.log('No winner yet.');
 }
-GameBoard.printInfo();
